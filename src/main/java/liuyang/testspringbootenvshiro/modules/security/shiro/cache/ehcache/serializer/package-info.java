771f2b57652656d64b47d7d050a5484c4ebd68c5
xconfigurator <xconfigurator@hotmail.com>
2021-09-06 19:14:02 +0800

    sync 202109061913

---
+++ src/main/java/liuyang/testspringbootenvshiro/modules/security/shiro/cache/ehcache/serializer/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 貌似使用Ehcache是不需要额外的序列化与反序列化动作的。
+ *
+ * @author liuyang
+ * @scine 2021/9/6
+ */
+package liuyang.testspringbootenvshiro.modules.security.shiro.cache.ehcache.serializer;
--- src/main/java/liuyang/testspringbootenvshiro/modules/security/shiro/config/ShiroConfig.java
+++ src/main/java/liuyang/testspringbootenvshiro/modules/security/shiro/config/ShiroConfig.java
@@ -29,6 +29,7 @@ import org.crazycake.shiro.RedisSessionDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -55,6 +56,7 @@ import java.util.List;
  * 2. SecurityManger <-- manages all Subjects
  * 3. Realm <-- access your security data
  */
+@ConditionalOnProperty(prefix = "liuyang", name = "security.shiro.enabled", havingValue = "true")
 @Configuration
 @Slf4j
 public class ShiroConfig {
